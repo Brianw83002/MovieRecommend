cdeef60f79c24a34bdda67c47c7f0e638dbe5a4f
Brianw83002 <brianw83002@gmail.com>
2025-11-25 09:15:32 -0800

    start

---
+++ MovieRecApp/MovieRecAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecAppApp.swift
+//  MovieRecApp
+//
+//  Created by csuftitan on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieRecAppTests/MovieRecAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecAppTests.swift
+//  MovieRecAppTests
+//
+//  Created by csuftitan on 11/24/25.
+//
+
+import Testing
+@testable import MovieRecApp
+
+struct MovieRecAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
